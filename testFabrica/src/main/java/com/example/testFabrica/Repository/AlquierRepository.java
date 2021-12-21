c3d7b5ff2cd60149630b1e4878c6de0e44a6853d
jhoanmoncada <jhoanmoncada@wposs.com>
2021-12-21 16:20:21 -0500

    Interface:
     - Se crean las interface para hacer uso de Jpa en el proyecto.

---
+++ testFabrica/src/main/java/com/example/testFabrica/Repository/AlquierRepository.java
@@ -0,0 +1,9 @@
+package com.example.testFabrica.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.testFabrica.Model.Alquiler;
+
+public interface AlquierRepository extends JpaRepository <Alquiler,Integer>{
+
+}
---
+++ testFabrica/src/main/java/com/example/testFabrica/Repository/JuegoRepository.java
@@ -0,0 +1,9 @@
+package com.example.testFabrica.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.testFabrica.Model.Juego;
+
+public interface JuegoRepository extends JpaRepository <Juego,Integer>{
+
+}
